202f2d79cc58d0d73ae3085c15bb717cdfd769a8
Victor Augusto <victor.augusto@discente.univasf.edu.br>
2022-03-29 15:39:39 -0300

    last changes

--- school/admin.py
+++ school/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from .models import Chamada,StudentExtra,TeacherExtra,Notice
-# Register your models here. (by sumit.luv)
+
 class StudentExtraAdmin(admin.ModelAdmin):
     pass
 admin.site.register(StudentExtra, StudentExtraAdmin)
